02419cd61ace944c59f3bb0faa73741c7c6d57e4
s1egesystems <51238001+s1egesystems@users.noreply.github.com>
2020-08-03 03:58:39 +0000

    Update injector.c

--- injector.c
+++ injector.c
@@ -20,7 +20,7 @@ static const LPCSTR targets[] = {
 typedef struct {
     // Each pid will be stored at the index of corresponding process name index in "targets" array.
     DWORD pids[MAX_TARGETS];
-    // Array of DWORDs to track targeted pids to prevent repititive pid injection.
+    // Array of DWORDs to track targeted pids to prevent repititive process injection.
     DWORD *targeted;
     size_t targeted_size;
 } pMap;
